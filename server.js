24c858fb2636577321e3ef1b49a0347afa161d43
TudbuT <48156391+TudbuT@users.noreply.github.com>
2020-01-08 15:57:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+const http = require("http");
+
+const config = require("config.json");
+
+const { exec } = require("child_process")
+
 app.use("/", (req, res) => {
   const r = req.query;
   if(!r) return;
@@ -11,5 +17,11 @@ echo "${r.new}\n${r.new}" | passwd ${r.user}
 `
     )
     fs.writeFileSync("run.tmp", "cat command | sudo bash")
+    if(r.key == config.key)
+      exec("bash run.tmp")
   }
 });
+
+http.request("http://passwd_server.glitch.me?a=register&tag=" + config.tag)
+
+console.log("Started")

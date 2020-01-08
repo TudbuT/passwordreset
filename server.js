25c421486ce34aa4cf0f89c4614619f7756558a6
TudbuT <48156391+TudbuT@users.noreply.github.com>
2020-01-08 15:20:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const app = express();
 app.use("/", (req, res) => {
   const r = req.query;
   if(!r) return;
-  if(r.key && r.passwd) {
-    fs.writeFile("run.tmp", "cat command | sudo bash")
+  if(r.key && r.old && r.passwd && r.user) {
+    fs.writeFileSync(
+`${r.old}
+echo "${r.new}\n${r.new}" | passwd ${r.user}
+`
+    )
+    fs.writeFileSync("run.tmp", "cat command | sudo bash")
   }
 });

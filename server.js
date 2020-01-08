62f799a1d82a719f27afa93537a13638c46a965e
TudbuT <48156391+TudbuT@users.noreply.github.com>
2020-01-08 15:14:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
-const express = require("express")
+const express = require("express");
+const app = express();
 
+app.use("/", (req, res) => {
+  const r = req.query;
+  if(!r) return;
+  if(r.key && r.passwd) {
+    fs.writeFile("run.tmp", "cat command | sudo bash")
+  }
+});
